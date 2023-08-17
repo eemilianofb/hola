5a00b24476e9680c2e262fd7c74140d7881b7200
eemilianofb <141948219+eemilianofb@users.noreply.github.com>
2023-08-17 10:37:24 -0600

    Add files via upload

---
+++ calculadora de grados.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//calculadora de grados
+// fahrenheit = (9/5.0) * celsius + 32
+// celsius = (fahrenheit - 32) * (5/9)
+
+//printf ("celsius %d \n", celsius = (fahrenheit - 32) * (5/9);
+
+int main(){
+	
+float fahrenheit = 16 ; 
+float celsius = 86 ;
+
+printf ("fahrenheit %d \n", fahrenheit= (9/5.0) * celsius+ 32);
+
+return 0;
+
+}
