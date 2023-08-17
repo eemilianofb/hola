bb3c8327e40fa3361878f316afd1a623b3641253
eemilianofb <141948219+eemilianofb@users.noreply.github.com>
2023-08-17 10:36:12 -0600

    Add files via upload

---
+++ Parciales c++.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define PI 3.141592
+
+//int main() {
+//printf("hola mundo");
+//printf("color %s, number %d, float %f","red", 154,3.14);
+
+//char letras[20] =" hola ";
+//int numeroEntero = 123;
+//float decimal = 3.14;
+//char letra = 'a';
+
+//printf ("entero %d    \n", numeroEntero);
+//printf ("flotante %f    \n", decimal);
+//printf ("letra %c    \n", letra);
+
+//printf("letra %c, number%d, float %f", letra, numeroEntero, decimal);
+
+/*int numero = 5;
+printf ("entero %d \n", numero);
+numero = 5 + numero;
+printf ("entero %d \n", numero);
+numero += 1;
+printf ("entero %d \n", numero);
+numero ++;
+printf ("entero %d \n", numero);
+numero = 1 + numero;
+printf ("entero %d \n", numero);
+
+printf ("PI %f", PI);
+
+return 0;*/
+
+int main(){
+	
+	float parcial1 = 9.5;
+	float parcial2 = 8.5;
+	
+	printf("parl %f  : par2 % \n" p)
+}
+
+}
+
+
+
+
