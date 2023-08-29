2c2e6ec28cb812c717dfc428c29705c5939fa913
eemilianofb <141948219+eemilianofb@users.noreply.github.com>
2023-08-29 09:33:32 -0600

    Add files via upload

---
+++ Operadores y expresiones 1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+ int main(){
+ 	
+ 	int a=5,b=6, potenciaValores, moduloValores;
+ 	
+ 	potenciaValores= pow(a,b);
+ 	printf("la potencia de los valores es %d\n", potenciaValores);
+ 	
+ 	moduloValores= (a%b);
+ 	printf("el modulo da como resultado %d\n", moduloValores);
+ 	
+ 	return 0;
+ 	
+ }
+
+
---
+++ Programa que calcula algunas expresiones logicas.cpp
@@ -0,0 +1,13 @@
+/*Programa que calcula algunas expresiones logicas*/
+
+#include<stdio.h>
+int main(){
+	
+	printf("expresion logica 1: %d\n", (5 > 3 && 10 <= 10));
+	printf("expresion logica 2: %d\n", (50 > 75 || 100 < 57));
+	printf("expresion logica 3: %d\n", (5 >= 3));
+	printf("expresion logica 4: %d\n", !(5 >= 3));
+	printf("expresion logica 5: %d\n", (5 >= 3));
+}
+
+
