23c3fe64e59a419248b64443c34533f53cc434a3
eemilianofb <141948219+eemilianofb@users.noreply.github.com>
2023-08-31 10:33:52 -0600

    Add files via upload

---
+++ arbolito.cpp
@@ -0,0 +1,42 @@
+//arbolito
+
+#include <stdio.h>
+
+int main ()
+{
+	int n;
+	
+	printf("Dame un numero del 1 al 10. \n");
+	scanf("%d",&n);
+	
+	if(n>=1 && n<=3);
+	{
+	printf("     +   \n");
+	printf("  //  \\\\ \n");
+	printf(" //    \\\\ \n");
+	printf("//      \\\\ \n");
+	printf(" ________     \n");
+	printf("    ||     \n");
+}
+else 
+{ 
+ if(n>=4 && n<=7);
+	 {
+     	printf("         +   \n");
+    	 printf("      //  \\\\ \n");
+     	printf("     //    \\\\ \n");
+	 	printf("    //      \\\\ \n");
+	 	printf("   //        \\\\ \n");
+	 	printf("  //          \\\\ \n");
+	 	printf(" //            \\\\ \n");
+	 	printf("//              \\\\ \n");
+     	printf("     ________     \n");
+	 	printf("        ||     \n");
+    }
+	
+	
+}
+
+	
+	return 0;
+}
